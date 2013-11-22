ab9fb60057c2644458590a404394e81cb53719ad
Justin Le <mstksg@gmail.com>
2013-11-22 14:27:47 -0800

    further readme and help details

--- README.md
+++ README.md
@@ -4,14 +4,16 @@ Connection Logger
 Short shell/bash script that might not have even been worth writing.
 
 Continually pings the given server at the given interval and logs a
-timestamped latency (or "n/a" if timeout) to stdout.
+timestamped latency (or "n/a" if timeout) to stdout.  CTRL+C or kill to stop.
 
 Useful for monitoring when your personal server goes offline.
 
 Usage:
 
-    connection-logger.sh PERIOD SERVER TIMEOUT
+    connection-logger.sh PERIOD HOST TIMEOUT
 
 Defaults to 15 second period, 8.8.8.8 (Google's DNS Nameserver), and 2 second
 timeout.
+
+`-h` for further details.
 
--- connection-logger.sh
+++ connection-logger.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+VERSION="1.0.0"
+
+usage() {
+  cat <<-ENDOFUSAGE
+
+  $(basename $0) [period] [host] [timeout]
+
+  Constantly logs the latency of a ping to the given server to stdout, with a
+  timestamp.  Useful for monitoring a personal server's connectivity.  CTRL+C
+  or kill to quit.
+
+    [period]  Time in seconds betwen the start of each ping.  Defaults to 10.
+
+    [host]    Hostname/IP address over server to ping.  Defaults to 8.8.8.8,
+                  Google's DNS nameserver.
+
+    [timeout] The timeout for each ping.  The latency of a timedout ping shows
+                  up as "n/a" on the log.  Defaults to 2.
+
+  Written by Justin Le (justin@jle.im) 2013
+  Version $VERSION
+
+ENDOFUSAGE
+  exit 0
+}
+
+while getopts ":h" Option; do
+  case $Option in
+    h)
+      usage
+      exit 1;;
+  esac
+done
+shift $(($OPTIND - 1))
+
+
 PERIOD=${1:-15}             # Delay between pings (seconds)
 SERVER=${2:-"8.8.8.8"}      # Server to ping; default: google's DNS nameserver
 TIMEOUT=${3:-2}             # Timeout to wait for each ping (seconds)
